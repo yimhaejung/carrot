5a395ee339c7d9825020ea84eeb934b3ba23893b
yimhaejung <kkaanm123@naver.com>
2020-09-09 16:45:23 +0900

    work done: setting develop env in mac os

---
+++ mac_side/00_HelloWorld/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+  int nA = 0;
+  printf("please input a single number:\n");
+  scanf("%d", &nA);
+
+  printf("your input number is %d \n", nA);
+  return 0;
+}
