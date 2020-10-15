2c88c33bd480e853f08da103bef60f50604ac10b
yimhaejung <kkaanm123@naver.com>
2020-10-15 17:15:27 +0900

    6-2 in progress

---
+++ ReportFile/report#6/6-2/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void swap(double *pa, double *pb);
+void line_up(double *maxp, double *midp, double *minp);
+
+int main(void)
+{
+  double max, mid, min;
+
+  printf("실수값 3개 입력 : ");
+  scanf("%lf%lf%lf", &max, &mid, &min);
+  line_up(&max, &mid, &min);
+  printf("정렬된 값 출력 : %.1lf, %.1lf, %.1lf\n", max, mid, min);
+
+  return 0;
+}
+
+void swap(double *pa, double *pb)
+{
+  double temp;
+  temp = *pa;
+  *pa = *pb;
+  *pb = temp;
+}
+void line_up(double *maxp, double *midp, double *minp)
+{
+  swap(maxp, minp);
+  swap(midp, minp); 
+}
