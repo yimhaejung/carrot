65d2ae8c30aad57f2ac448ab2c1ece0d3cc940e5
yimhaejung <kkaanm123@naver.com>
2020-10-26 17:02:49 +0900

    report#7 completed

--- ReportFile/prac/main.c
+++ ReportFile/prac/main.c
@@ -3,24 +3,17 @@
 #include <time.h>
 #include <string.h>
 
-void print_month(int *mp);
+int *sum(int a, int b)
+{
+  static int res;
+  res = a + b;
+  return res;
+}
 int main(void)
 {
-  int month[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-  print_month(month);
+  int *resp;
+  resp = sum(10, 20);
+  printf("gkq : %d\n", *resp);
 
   return 0;
-}
-
-void print_month(int *mp)
-{
-  int i;
-  for(i = 0; i < 12; i++)
-  {
-    printf("%5d", mp[i]);
-    if((i + 1) % 5 == 0)
-    {
-      printf("\n");
-    }
-  }
 }
